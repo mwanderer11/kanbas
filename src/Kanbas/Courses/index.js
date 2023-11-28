30458845393798331f749e17ce41d66bce065a8a
maliawanderer <malia.wanderer@gmail.com>
2023-11-28 10:57:52 -0500

    courses url

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -9,7 +9,7 @@ import Modules from "./Modules";
 
 function Courses({courses}) {
     const {courseId} = useParams();
-    const URL = "http://localhost:4000/api/courses";
+    const URL = 'https://kanbas-node-server-app-d13q.onrender.com/api/courses';
 
     const [course, setCourse] = useState({});
     const findCourseById = async (courseId) => {
