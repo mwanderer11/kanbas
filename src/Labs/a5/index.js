e61d1272c64ea4777c31268909ff85f47ca83074
maliawanderer <malia.wanderer@gmail.com>
2023-11-28 15:07:30 -0500

    render link

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -6,6 +6,9 @@ function Assignment5() {
     return (
         <div>
             <h1>Assignment 5</h1>
+            <a href="https://kanbas-node-server-app-d13q.onrender.com">
+                I forgot to include the link to the render site in my submission so I'm adding it here
+            </a>
             <div className="list-group">
                 <a href="localhost:4000/a5/welcome"
                    className="list-group-item">
