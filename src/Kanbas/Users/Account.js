4ad617975cd3f1faa31786636b022f5aea5a9a0f
maliawanderer <malia.wanderer@gmail.com>
2023-12-06 23:12:22 -0500

    typo

--- src/Kanbas/Users/Account.js
+++ src/Kanbas/Users/Account.js
@@ -57,7 +57,7 @@ function Account() {
                     </Link>
                 </div>
             )}
-            <button onClick{signin} className="btn btn-danger">Sign in</button>
+            <button onClick={signin} className="btn btn-danger">Sign in</button>
         </div>
     );
 }
